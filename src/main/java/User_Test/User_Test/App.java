36fdf58458adfc6a691a8d5c47626a2c4c26ad8b
shyam <shyam@DESKTOP-GJF8DS5>
2020-04-30 12:26:47 -0700

    Test is modified.

---
+++ src/main/java/User_Test/User_Test/App.java
@@ -0,0 +1,13 @@
+package User_Test.User_Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/User_Test/User_Test/USet_test.java
@@ -0,0 +1,29 @@
+package User_Test.User_Test;
+
+import org.testng.annotations.Test;
+
+public class USet_test 
+{
+	@Test
+	public void CreateUserTest()
+	{
+		System.out.println(" User_test_created");
+	}
+	
+	@Test
+	public void ModifyUserTest()
+	{
+		System.out.println(" Modify_User_Test");
+	}
+	
+	@Test
+	public void delete_User_Test()
+	{
+		System.out.println(" delete_User_Test");
+	}
+	@Test
+	public void delete_User_Test2()
+	{
+		System.out.println(" delete_User_Test2");
+	}
+}
